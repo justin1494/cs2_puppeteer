b2e2679aa3dc9421990d17a53c823be95af0f54a
justin1494 <m.jaskolowski1994@gmail.com>
2024-08-05 22:56:50 +0200

    added codel ines from puppeteer docker

--- index.js
+++ index.js
@@ -44,7 +44,18 @@ async function waitForMatchesToLoad(page) {
 async function loginToLeetify() {
   let browser;
   try {
-    browser = await getBrowser();
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
 
     // Navigate to the Leetify login page
